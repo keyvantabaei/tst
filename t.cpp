e31e5b90ddf34e9574cce651a03d132560206cba
keyvantabaei <keyvantabaei74@gmail.com>
2023-02-09 13:30:48 -0800

    ss

--- t.cpp
+++ t.cpp
@@ -4,11 +4,8 @@ using namespace std;
 int main()
 {
 	cout<< "Hello wordl from VIM"<<endl;
-<<<<<<< HEAD
 	cout<< "this is for git"<<endl;
 	cout<< "this is for new branch"<<endl;
 	cout<< "this is for master"<<endl;
-=======
->>>>>>> newb
 	return 0;
 }

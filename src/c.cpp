8948b455eeee657b9826eb7446126eba51566e09
keyvantabaei <keyvantabaei74@gmail.com>
2023-02-09 01:33:36 -0800

    files

---
+++ src/c.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  c.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/09/23 01:28:11
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+	{
+		cout<<"from src hello !"<<endl;
+		return 0;
+	}
+
